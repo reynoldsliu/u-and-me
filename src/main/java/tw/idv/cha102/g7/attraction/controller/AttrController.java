bc4a94360ebb74b3b91e64c5de9d8a6b03c9bf2c
reynolds <reynoldsliu01443@gmail.com>
2023-08-28 01:59:58 +0800

    add attr html compiled del html from templates compiled upd attr java compiled upd attr html compiled

--- src/main/java/tw/idv/cha102/g7/attraction/controller/AttrController.java
+++ src/main/java/tw/idv/cha102/g7/attraction/controller/AttrController.java
@@ -47,19 +47,6 @@ public class AttrController {
         return "index";
     }
 
-//    @RequestMapping("/AttractionPage")
-//    public void AttractionPage(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-//        String url = "/AttractionPage";
-//
-//
-//        RequestDispatcher successView = req.getRequestDispatcher(url); // 成功轉交 listOneEmp.jsp
-//        successView.forward(req, res);
-//
-//    }
-
-//    @Autowired
-//    private ResourceLoader resourceLoader;
-
     @RequestMapping("/AttractionPage")
     public void AttractionPage(HttpServletRequest req,HttpServletResponse res) throws ServletException, IOException {
         String redirectUrl = "AttractionPage.html"; // 設定要重定向的目標 URL
@@ -73,48 +60,12 @@ public class AttrController {
         res.sendRedirect(redirectUrl);
     }
 
-//    @GetMapping("/AttractionPage")
-//    public ResponseEntity<String> AttractionPage(HttpServletResponse res) throws IOException {
-//        String url = "{\"redirect\": \"AttractionPage.html\"}";
-//        System.out.println("printprintprintprint"+ResponseEntity.ok(url));
-//        return ResponseEntity.ok(url);
-//         //res.sendRedirect("AttractionPage");
-//    }
-
-
-        @GetMapping("/register")
-    public String register(Model model) {
-        model.addAttribute("pageTitle", "Thymeleaf Example");
-        model.addAttribute("message", "Hello, Thymeleaf!");
-        return "register";
-    }
-
-//    @GetMapping("/login2")
-//    public String loginExample(Model model) {
-//        model.addAttribute("pageTitle", "Login2 Example");
-//        model.addAttribute("message", "Hello, Login2!");
-//        return "login2";
-//    }
 
 
     @RequestMapping("/getAttr")
     public Attraction getAttr(@RequestParam Integer attrId) {
         return attrService.getById(attrId);
     }
-
-
-//    @RequestMapping("/getOneAttr")
-//    public void getOneAttr(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-//
-//        String url = "/jsp/getOneAttr.jsp";
-//
-//        Attraction attraction = attrService.getById(1);
-//        HttpSession session = req.getSession();
-//        session.setAttribute("attraction", attraction);
-//
-//        RequestDispatcher successView = req.getRequestDispatcher(url); // 成功轉交 listOneEmp.jsp
-//        successView.forward(req, res);
-//    }
 
 
     //Spring MVC html轉向寫法
@@ -145,19 +96,6 @@ public class AttrController {
         return pageResult.getContent();
     }
 
-    @PostMapping("/getNumOfResult")
-    public Integer getNumOfResult(@RequestBody List<Attraction> attrList){
-        int count = 0;
-//        List<Attraction> list = attrList;
-        for(Attraction attr:attrList){
-            if(attrList.size()==0){
-                return count;
-            }
-            attrList.remove(attr);
-            count++;
-        }
-        return 0;
-    }
 
 
     //SERVLET 轉向寫法
@@ -172,36 +110,6 @@ public class AttrController {
 //        RequestDispatcher successView = req.getRequestDispatcher(url); // 成功轉交 listOneEmp.jsp
 //        successView.forward(req, res);
 //    }
-
-    @RequestMapping("/hello")
-    public void hello(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-        String url = "/jsp/hello.jsp";
-
-        RequestDispatcher successView = req.getRequestDispatcher(url); // 成功轉交 listOneEmp.jsp
-        successView.forward(req, res);
-
-    }
-
-//    @RequestMapping("/index")
-//    public void ajax(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-//        String url = "/jsp/index.jsp";
-//
-//        RequestDispatcher successView = req.getRequestDispatcher(url); // 成功轉交 listOneEmp.jsp
-//        successView.forward(req, res);
-//    }
-
-//    @RequestMapping("/yt")
-//    public void showYtPage(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-//        String url = "/jsp/yt_layout.jsp";
-//        RequestDispatcher successView = req.getRequestDispatcher(url); // 成功轉交 listOneEmp.jsp
-//        successView.forward(req, res);
-//    }
-
-    @RequestMapping("/yt")
-    public String goto_yt(Model model) {
-
-        return "yt_layout";
-    }
 
     @Autowired
     private ObjectMapper objectMapper;
@@ -221,29 +129,6 @@ public class AttrController {
         String jsonString = objectMapper.writeValueAsString(attraction);
 
         return attraction;
-    }
-
-    @RequestMapping("/bootstrap12")
-    public String goto_bootstrap12(Model model) {
-
-        return "bootstrap12";
-    }
-
-    @GetMapping("/getAttraction/{id}")
-    public String getAttractionById(@PathVariable Integer id) throws JsonProcessingException {
-        Attraction attraction = attrService.getById(id);
-        System.out.println("in");
-        System.out.println(attraction);
-        if (attraction != null) {
-            String attractionJson = JSONObject.toJSONString(attraction);
-//            JSONObject.
-            return attractionJson;
-//            writePojo2Json(ResponseEntity<attraction>,attraction);
-//
-//            return ResponseEntity.ok(attraction.toString());
-        } else {
-            return null;
-        }
     }
 
     @Autowired
