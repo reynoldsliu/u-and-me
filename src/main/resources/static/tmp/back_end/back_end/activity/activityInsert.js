deb75e25ea64631b6b47f4911b98a8e9953f3d74
Katie <pooh19960408@gmail.com>
2023-09-06 00:41:02 +0800

    update ActivityIndex/News/Periods/Psytest, activityEdit/List.html/js/css compiled

---
+++ src/main/resources/static/tmp/back_end/back_end/activity/activityInsert.js
@@ -0,0 +1,134 @@
+// 表單中的各輸入框元素
+const activIdInput = document.getElementById('activId');
+const activPicImage = document.getElementById('activPic');
+const activNameInput = document.getElementById('activName');
+const activConTextarea = document.getElementById('activCon');
+const activStarttimeInput = document.getElementById('activStarttime');
+const activEndtimeInput = document.getElementById('activEndtime');
+const activStaSelect = document.querySelector('.form-control.select2');
+const submitBtn = document.getElementById('addButton');
+let cover;
+let activSta;
+
+
+
+// 網頁載入後執行
+document.addEventListener('DOMContentLoaded', function () {
+    // 點選圖片觸發事件
+    const cover_el = document.getElementById('inputGroupFile04');
+    const cover_img = document.getElementById('activPic');
+
+    cover_el.addEventListener('change', function () {
+        const file = this.files[0];
+        if (file) {
+            cover_img.src = URL.createObjectURL(file);
+
+            const fileReader = new FileReader();
+            fileReader.onload = event => {
+                cover = btoa(event.target.result);
+            };
+            fileReader.readAsBinaryString(file);
+        } else {
+            cover_img.src = "";
+        }
+    });
+
+// 獲取當前日期
+    const currentDateTime = new Date().toISOString().slice(0, 16);
+    activStarttimeInput.setAttribute('min', currentDateTime);
+
+    activStarttimeInput.addEventListener('change', function () {
+        const startTime = new Date(activStarttimeInput.value);
+        const endTime = new Date(activEndtimeInput.value);
+
+        // 更新結束時間輸入框最小值為活動開始時間
+         activEndtimeInput.setAttribute('min', activStarttimeInput.value);
+
+        if (endTime <= startTime) {
+            // 將結束時間重置為開始時間的值
+            activEndtimeInput.value = activStarttimeInput.value;
+        }
+    });
+
+
+    activEndtimeInput.addEventListener('change', function () {
+        const startTime = new Date(activStarttimeInput.value);
+        const endTime = new Date(activEndtimeInput.value);
+
+        if (endTime <= startTime) {
+
+            activEndtimeInput.value = activStarttimeInput.value;
+        }
+    });
+
+    // 點擊按鈕後新增活動數據
+    submitBtn.addEventListener('click', async function (event) {
+        event.preventDefault();
+
+        // 驗證
+        if (!cover || !activNameInput.value || !activConTextarea.value || !activStarttimeInput.value || !activEndtimeInput.value || !activStaSelect.value) {
+            Swal.fire({
+                icon: 'warning',
+                title: '請填寫所有欄位',
+                text: '',
+                confirmButtonText: '確定'
+            });
+            return; // 阻止繼續進行更新操作
+        }
+
+        const startTime = new Date(activStarttimeInput.value);
+        const endTime = new Date(activEndtimeInput.value);
+
+
+        // 構建要傳遞的資料
+        const newActivityData = {
+            activPic: cover,
+            activName: activNameInput.value,
+            activCon: activConTextarea.value,
+            activStarttime: startTime.toISOString(),
+            activEndtime: endTime.toISOString(),
+            activSta: activStaSelect.value
+        };
+
+        try {
+            const response = await fetch('http://localhost:8080/u-and-me/add', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(newActivityData)
+            });
+
+            if (response.ok) {
+                const updatedActivityData = await response.json();
+
+                Swal.fire({
+                    icon: 'success',
+                    title: '更新成功',
+                    text: '',
+                    confirmButtonText: '確定'
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        window.location.href = 'activityList.html';
+                    }
+                });
+
+            } else {
+
+                Swal.fire({
+                    icon: 'error',
+                    title: '更新失敗',
+                    text: '',
+                    confirmButtonText: '確定'
+                }).then((result) => {
+                    if (result.isConfirmed) {
+
+                    }
+                });
+                console.error('Error adding activity:', response.statusText);
+            }
+        } catch (error) {
+            console.error('Error adding activity:', error);
+        }
+    });
+});
--- src/main/resources/static/tmp/back_end/back_end/activity/activityList.js
+++ src/main/resources/static/tmp/back_end/back_end/activity/activityList.js
@@ -1,17 +1,87 @@
 /* global bootstrap: false */
 (function () {
-  'use strict'
-  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-  tooltipTriggerList.forEach(function (tooltipTriggerEl) {
-    new bootstrap.Tooltip(tooltipTriggerEl)
+    'use strict'
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    tooltipTriggerList.forEach(function (tooltipTriggerEl) {
+        new bootstrap.Tooltip(tooltipTriggerEl)
 
-    var dropdownToggleList = [].slice.call(document.querySelectorAll('[data-bs-toggle="dropdown"]'))
-    dropdownToggleList.map(function (dropdownToggle) {
-      return new bootstrap.Dropdown(dropdownToggle)
-    });
-    
-
+        var dropdownToggleList = [].slice.call(document.querySelectorAll('[data-bs-toggle="dropdown"]'))
+        dropdownToggleList.map(function (dropdownToggle) {
+            return new bootstrap.Dropdown(dropdownToggle)
+        });
 
 
-  })
+
+
+    })
 })()
+
+// ----------------------------------------------------------------------------------------------
+
+// <!--網頁載入後執行-->
+document.addEventListener("DOMContentLoaded", function () {
+    fetchActivityList();
+});
+
+async function fetchActivityList() {
+    try {
+        const response = await fetch('http://localhost:8080/u-and-me/activityall');
+        const activityList = await response.json();
+
+        const dataTableList = document.getElementById("dataTableList");
+        dataTableList.innerHTML = "";
+
+        activityList.forEach(activity => {
+            const row = document.createElement("tr");
+            row.innerHTML = `
+                <td style="width: 60px;text-align: center; vertical-align: middle;">${activity.activId}</td>
+                <td style="width: 100px;text-align: center;vertical-align: middle;">${activity.activName}</td>
+                <td style="vertical-align: middle;">${activity.activCon}</td>
+                <td style="width: 120px;text-align: center;vertical-align: middle;">${formatDate(activity.activStarttime)}</td>
+                <td style="width: 120px;text-align: center;vertical-align: middle;">${formatDate(activity.activEndtime)}</td>
+                <td style="width: 65px;text-align: center;vertical-align: middle;">${statusMapping.get(activity.activSta)}</td>
+                <td style="width: 65px;text-align: center;vertical-align: middle;">
+                 <button class="btn btn-outline-success" onclick="redirectToDetailPage(${activity.activId})">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"fill="currentColor" class="bi bi-pen" viewBox="0 0 16 16">
+                  <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001zm-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z"/>
+                  </svg>
+                  </button>
+                </td>
+            `;
+            dataTableList.appendChild(row);
+        });
+
+    } catch (error) {
+        console.error("Error fetching activity list:", error);
+    }
+}
+
+
+//function formatDate(dateString) {
+//      const options = { year: 'numeric', month: 'long', day: 'numeric' };
+//      return new Date(dateString).toLocaleDateString('zh-TW', options);
+//}
+
+
+function formatDate(dateString) {
+    const options = { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit' };
+    const formattedDate = new Date(dateString).toLocaleString('zh-TW', options).replace(/\//g, '-');
+
+    return formattedDate.replace(' ', '\n');
+}
+
+const formattedDate = formatDate('2023-09-01T00:30:00');
+console.log(formattedDate); // 输出格式为 "2023-09-01 14:30"
+
+
+
+const statusMapping = new Map([
+    [0, '下架'],
+    [1, '上架'],
+]);
+
+// <!--按修改鈕會根據activityId跳轉到詳細內容頁面，並將資料映射到相關欄位上-->
+function redirectToDetailPage(activId) {
+    var newPageUrl = `activityEdit.html?activId=${activId}`;
+    window.location.href = newPageUrl;
+}
