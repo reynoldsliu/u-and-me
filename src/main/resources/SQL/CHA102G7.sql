be044a1ecf625049f90cee74839c81b42ea07980
Shin-An <siancat0525@gmail.com>
2023-09-06 22:27:49 +0800

    upd Front schedule files .html compiled upd G7's sql compiled

--- src/main/resources/SQL/CHA102G7.sql
+++ src/main/resources/SQL/CHA102G7.sql
@@ -25,7 +25,7 @@ VALUES
   (1,'member1@example.com', 'password1', 'tong', 2, 'Address 1', 1,'A123456789','0911111111', 100, 1, 0),
   (2,'member2@example.com', 'password2', 'lynn', 2, 'Address 2', 2,'B123456789', '0922222222', 400, 0, 0),
   (3,'member3@example.com', 'password3', 'sian', 2, 'Address 3', 1,'C123456789','0933333333', 550, 1, 0),
-  (4,'member4@example.com', 'password4', 'reynolds', 1, 'Address 4','D123456789', '0944444444', 300, 2, 1),
+  (4,'member4@example.com', 'password4', 'reynolds', 1, 'Address 4',1,'D123456789', '0944444444', 300, 2, 1),
   (5,'member5@example.com', 'password5', 'Katie', 2, 'Address 5', 2,'E123456789', '0955555555', 250, 1, 1),
   (6,'member6@example.com', 'password6', 'jas', 2, 'Address 6', 1,'F123456789','0966666666', 50, 0, 0),
   (7,'member7@example.com', 'password7', 'henry', 1, 'Address 7', 3,'G123456789', '0977777777', 50, 1, 0),
@@ -177,20 +177,6 @@ values
     (108, 108, 208, 4, 3, 8, 1000, 'Photography Tour', '2023-11-12 09:00:00', '2023-11-18 17:00:00', '2023-11-01 23:59:59', 'Capture stunning landscapes and wildlife with photography', 'Bring your own camera gear', 0, 0, null),
     (109, 109, 209, 6, 4, 10, 1200, 'Foodie Adventure', '2023-10-05 12:00:00', '2023-10-15 20:00:00', '2023-09-25 23:59:59', 'Explore various cuisines and food spots', 'Come hungry and ready to eat', 0, 0, null),
     (110, 110, 210, 3, 2, 5, 700, 'Fishing Trip', '2023-11-25 07:00:00', '2023-11-28 15:00:00', '2023-11-15 19:00:00', 'Enjoy fishing in serene lakes and rivers', 'Fishing equipment will be provided', 0, 0, null);
-
-
-insert into `group`
-values
-    (101, 101, 201, 5, 3, 8, 1000, 'Hiking Trip', '2023-08-10 09:00:00', '2023-08-15 12:00:00', '2023-08-01 18:00:00', 'A fun hiking trip to the mountains', 'Bring your own water and snacks', 0),
-    (102, 102, 202, 4, 2, 6, 800, 'Beach Vacation', '2023-09-05 14:00:00', '2023-09-12 10:00:00', '2023-08-25 23:59:59', 'Relax and unwind at the sunny beach', 'Sunscreen is a must', 0),
-    (103, 103, 203, 6, 4, 10, 1200, 'City Tour', '2023-07-20 11:00:00', '2023-07-25 16:00:00', '2023-07-15 22:00:00', 'Explore the city and visit popular landmarks', 'Comfortable shoes are recommended', 0),
-    (104, 104, 204, 3, 2, 4, 600, 'Camping Adventure', '2023-08-28 15:00:00', '2023-09-02 09:00:00', '2023-08-20 20:00:00', 'Experience the great outdoors with camping', 'Bring your own tent and sleeping bag', 0),
-    (105, 105, 205, 7, 5, 12, 1500, 'Cruise Journey', '2023-10-15 13:00:00', '2023-10-25 18:00:00', '2023-10-01 19:00:00', 'Enjoy a luxurious cruise experience', 'Formal attire required for dinner', 0),
-    (106, 106, 206, 2, 2, 4, 400, 'Skiing Adventure', '2023-12-05 08:00:00', '2023-12-10 16:00:00', '2023-11-25 23:59:59', 'Hit the slopes and have fun skiing', 'Skiing equipment available for rent', 0),
-    (107, 107, 207, 8, 6, 15, 1800, 'Road Trip', '2023-09-30 10:00:00', '2023-10-10 14:00:00', '2023-09-15 18:00:00', 'Embark on an exciting road trip across the country', 'Prepare some road trip playlists', 0),
-    (108, 108, 208, 4, 3, 8, 1000, 'Photography Tour', '2023-11-12 09:00:00', '2023-11-18 17:00:00', '2023-11-01 23:59:59', 'Capture stunning landscapes and wildlife with photography', 'Bring your own camera gear', 0),
-    (109, 109, 209, 6, 4, 10, 1200, 'Foodie Adventure', '2023-10-05 12:00:00', '2023-10-15 20:00:00', '2023-09-25 23:59:59', 'Explore various cuisines and food spots', 'Come hungry and ready to eat', 0),
-    (110, 110, 210, 3, 2, 5, 700, 'Fishing Trip', '2023-11-25 07:00:00', '2023-11-28 15:00:00', '2023-11-15 19:00:00', 'Enjoy fishing in serene lakes and rivers', 'Fishing equipment will be provided', 0);
 select * from `group`;
 
 
@@ -468,9 +454,9 @@ primary key (prod_id, mem_id)
 create table product_picture(
 prodpic_id int primary key auto_increment,
 prod_id int not null,
-prod_pic mediumblob,
-constraint fk_prod_id
-foreign key(prod_id) references product (prod_id) 
+prod_pic mediumblob -- ,
+-- constraint fk_prod_id
+-- foreign key(prod_id) references product (prod_id)
 );
 
 -- 購物車清單 --
@@ -500,15 +486,15 @@ select * from product_category;
 select * from product;
 insert into product(prod_id,prodcat_id,prod_name,prod_con,prod_pri,prod_qty,prod_spec)
 values
-(101,2,'日月潭紅茶','台茶18號，茶湯呈現典雅金紅色，茶香中帶有淡淡薄荷肉桂香氣，此香氣更被世界紅茶專家譽為日月潭紅茶特有之「台灣香」',699,0,1,'3入'),
-(102,3,'設計師聯名款太陽眼鏡','UandMe與設計師太陽眼鏡款，擷取時尚與文化融入設計，絕對是旅行中不可或缺的配件 ',2999,2,1,'1入'),
-(103,4,'大自然的氣息','工作繁忙沒辦法安排假期旅行嗎? 把大自然的氣息帶回去，讓你在家也能有沉浸式的旅行體驗',11111,10,1,'1入'),
-(104,2,'冠軍烘培咖啡豆','COFFEE',1080,10,1,'2入'),
-(105,1,'原住民手工編織包','BAGS',1680,10,1,'1入'),
-(106,3,'多功能行動電源','POWERBANK',1080,10,1,'10入'),
-(107,4,'質感手提行李箱','LUGGAGE',3680,10,1,'10入'),
-(108,1,'手沖咖啡旅行組','COFFEEEEE',2980,10,0,'10入'),
-(109,1,'露營必備裝飾燈串','LIGHT',480,10,0,'1入');
+(101,2,'日月潭紅茶','台茶18號，茶湯呈現典雅金紅色，茶香中帶有淡淡薄荷肉桂香氣，此香氣更被世界紅茶專家譽為日月潭紅茶特有之「台灣香」',699,1,'3入'),
+(102,3,'設計師聯名款太陽眼鏡','UandMe與設計師太陽眼鏡款，擷取時尚與文化融入設計，絕對是旅行中不可或缺的配件 ',2999,1,'1入'),
+(103,4,'大自然的氣息','工作繁忙沒辦法安排假期旅行嗎? 把大自然的氣息帶回去，讓你在家也能有沉浸式的旅行體驗',11111,1,'1入'),
+(104,2,'冠軍烘培咖啡豆','COFFEE',1080,1,'2入'),
+(105,1,'原住民手工編織包','BAGS',1680,1,'1入'),
+(106,3,'多功能行動電源','POWERBANK',1080,1,'10入'),
+(107,4,'質感手提行李箱','LUGGAGE',3680,1,'10入'),
+(108,1,'手沖咖啡旅行組','COFFEEEEE',2980,0,'10入'),
+(109,1,'露營必備裝飾燈串','LIGHT',480,0,'1入');
 
 
 select * from product_collection;
