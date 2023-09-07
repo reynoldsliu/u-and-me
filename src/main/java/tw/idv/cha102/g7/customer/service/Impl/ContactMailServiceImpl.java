fbb1063d38f6eead8a40218d974de4cd56c534c8
Shin-An <siancat0525@gmail.com>
2023-09-08 01:04:41 +0800

    disabled comment contact compiled

--- src/main/java/tw/idv/cha102/g7/customer/service/Impl/ContactMailServiceImpl.java
+++ src/main/java/tw/idv/cha102/g7/customer/service/Impl/ContactMailServiceImpl.java
@@ -1,30 +1,30 @@
-package tw.idv.cha102.g7.customer.service.Impl;
-
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.stereotype.Service;
-import tw.idv.cha102.g7.customer.entity.ContentEmail;
-import tw.idv.cha102.g7.customer.service.ContactMailService;
-
-@Service
-public class ContactMailServiceImpl implements ContactMailService {
-
-    @Autowired
-    private JavaMailSender mailSender;
-
-    public void sendContactMail(ContentEmail contentEmail) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("xiaoina914@gmail.com");
-        message.setTo("xiaoina914@gmail.com");
-        message.setSubject("U-and-Me 客服郵件");
-        message.setText("寄件人姓名：" + contentEmail.getName()+"\n\n"+
-                "寄件人信箱：" + contentEmail.getUsermail()+"\n\n"+
-                "＊－。－。－。－。－ 寄件人留言 －。－。－。－。－＊\n\n"+
-                contentEmail.getContent()+"\n\n"+
-                "＊－。－。－。－。－。－。－。－－。－。－。－。－＊");
-
-        mailSender.send(message);
-    }
-}
+//package tw.idv.cha102.g7.customer.service.Impl;
+//
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.mail.SimpleMailMessage;
+//import org.springframework.mail.javamail.JavaMailSender;
+//import org.springframework.stereotype.Service;
+//import tw.idv.cha102.g7.customer.entity.ContentEmail;
+//import tw.idv.cha102.g7.customer.service.ContactMailService;
+//
+//@Service
+//public class ContactMailServiceImpl implements ContactMailService {
+//
+//    @Autowired
+//    private JavaMailSender mailSender;
+//
+//    public void sendContactMail(ContentEmail contentEmail) {
+//        SimpleMailMessage message = new SimpleMailMessage();
+//        message.setFrom("xiaoina914@gmail.com");
+//        message.setTo("xiaoina914@gmail.com");
+//        message.setSubject("U-and-Me 客服郵件");
+//        message.setText("寄件人姓名：" + contentEmail.getName()+"\n\n"+
+//                "寄件人信箱：" + contentEmail.getUsermail()+"\n\n"+
+//                "＊－。－。－。－。－ 寄件人留言 －。－。－。－。－＊\n\n"+
+//                contentEmail.getContent()+"\n\n"+
+//                "＊－。－。－。－。－。－。－。－－。－。－。－。－＊");
+//
+//        mailSender.send(message);
+//    }
+//}
