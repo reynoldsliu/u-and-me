bd3fca8f5ec516650c62e4668eff7d7b7ec2ee27
Lynn <pudding1426@gmail.com>
2023-09-14 04:01:21 +0800

    upd myOrder.js

--- src/main/resources/static/tmp/Front/shop/dist/css/myOrder.js
+++ src/main/resources/static/tmp/Front/shop/dist/css/myOrder.js
@@ -88,15 +88,15 @@ async function myOrderList(page) {
             console.log(orders);
             const row = document.createElement("tr");
             row.innerHTML = `
-    <td style="width: 50px;text-align: center; vertical-align: middle;">${orders.ordId}</td>
-    <td style="width: 130px;text-align: center;vertical-align: middle;">${formatDate(orders.ordTime)}</td>
-    <td style="width: 100px;text-align: center;vertical-align: middle;">${orders.checktotal}</td>
+    <td style="width: 40px;text-align: center; vertical-align: middle;">${orders.ordId}</td>
+    <td style="width: 100px;text-align: center;vertical-align: middle;">${formatDate(orders.ordTime)}</td>
+    <td style="width: 50px;text-align: center;vertical-align: middle;">${orders.checktotal}</td>
     <td style="width: 50px;text-align: center; vertical-align: middle;">${orders.recipientName}</td>
     <td style="width: 50px;text-align: center; vertical-align: middle;">${orders.recipientPhone}</td>
-    <td style="width: 50px;text-align: center; vertical-align: middle;">${orders.recipientAddr}</td>
+    <td style="width: 100px;text-align: center; vertical-align: middle;">${orders.recipientAddr}</td>
     <td style="width: 65px;text-align: center;vertical-align: middle;">${statusMapping.get(orders.ordSta)}</td>
     <td style="width: 65px;text-align: center;vertical-align: middle;">${ordPayStaMapping.get(orders.ordPaySta)}</td>
-                        <td style="width: 40px;text-align: center;vertical-align: middle;">
+    <td style="width: 40px;text-align: center;vertical-align: middle;">
         <button class="btn btn-outline-success" onclick="redirectToDetailPage(${orders.ordId})">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pen" viewBox="0 0 16 16">
                 <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001zm-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z" />
