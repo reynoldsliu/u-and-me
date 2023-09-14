5a1b08fa12b75f877b9128f1664bcf0a6a4b408e
Lynn <pudding1426@gmail.com>
2023-09-15 02:39:53 +0800

    add updateordSta method upd orderDetail.html/js upd myOrder compiled

--- src/main/resources/static/tmp/Front/shop/dist/css/myOrder.js
+++ src/main/resources/static/tmp/Front/shop/dist/css/myOrder.js
@@ -94,12 +94,13 @@ async function myOrderList(page) {
     <td style="width: 50px;text-align: center; vertical-align: middle;">${orders.recipientName}</td>
     <td style="width: 50px;text-align: center; vertical-align: middle;">${orders.recipientPhone}</td>
     <td style="width: 100px;text-align: center; vertical-align: middle;">${orders.recipientAddr}</td>
-    <td style="width: 65px;text-align: center;vertical-align: middle;">${statusMapping.get(orders.ordSta)}</td>
     <td style="width: 65px;text-align: center;vertical-align: middle;">${ordPayStaMapping.get(orders.ordPaySta)}</td>
+    <td style="width: 65px;text-align: center;vertical-align: middle;">${statusMapping.get(orders.ordSta)}</td>
     <td style="width: 40px;text-align: center;vertical-align: middle;">
         <button class="btn btn-outline-success" onclick="redirectToDetailPage(${orders.ordId})">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pen" viewBox="0 0 16 16">
-                <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001zm-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z" />
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clipboard2-check-fill" viewBox="0 0 16 16">
+            <path d="M10 .5a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5.5.5 0 0 1-.5.5.5.5 0 0 0-.5.5V2a.5.5 0 0 0 .5.5h5A.5.5 0 0 0 11 2v-.5a.5.5 0 0 0-.5-.5.5.5 0 0 1-.5-.5Z"/>
+            <path d="M4.085 1H3.5A1.5 1.5 0 0 0 2 2.5v12A1.5 1.5 0 0 0 3.5 16h9a1.5 1.5 0 0 0 1.5-1.5v-12A1.5 1.5 0 0 0 12.5 1h-.585c.055.156.085.325.085.5V2a1.5 1.5 0 0 1-1.5 1.5h-5A1.5 1.5 0 0 1 4 2v-.5c0-.175.03-.344.085-.5Zm6.769 6.854-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 9.793l2.646-2.647a.5.5 0 0 1 .708.708Z"/>
             </svg>
         </button>
     </td>
@@ -125,9 +126,7 @@ function formatDate(dateString) {
 
 const statusMapping = new Map([
     [0, '未出貨'],
-    [1, '已出貨'],
-    [2, '已到貨'],
-    [3, '訂單完成'],
+    [1, '訂單完成'],
 ]);
 
 const ordPayStaMapping = new Map([
@@ -136,9 +135,11 @@ const ordPayStaMapping = new Map([
 ]);
 
 
+
+
 // <!--按修改鈕會根據ordId跳轉到詳細內容頁面，並將資料映射到相關欄位上-->
 function redirectToDetailPage(ordId) {
-    alert("Go to OrderDetail page.")
+    // alert("Go to OrderDetail page.")
     var newPageUrl = baseUrl + `tmp/Front/shop/orderDetail.html?ordId=${ordId}`;
     window.location = newPageUrl;
 }
